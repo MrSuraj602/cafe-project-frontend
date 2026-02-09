e6e09ed3e417dca1da5d63af38cce6dccaaa5df7
MrSuraj602 <surajop602@gmail.com>
2026-02-09 15:59:45 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,15 @@ import '../styles/Footer.css'
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2026 The Appa's Café. All rights reserved.</p>
+      <p>Developed By : Suraj Rathod, Om Doshi, Sanskar Tathe</p>
+   
       <a href="https://www.google.com/search?q=the+appa%27s+cafe+&sca_esv=c6630c33b2d75486&biw=1536&bih=776&sxsrf=ANbL-n7Iyov9dE_B_I86qbSyfPZIwXU4KQ%3A1770632633931&ei=ubWJab-_OIaxwcsP_5_ziQY&ved=0ahUKEwj__7DJmMySAxWGWHADHf_PPGEQ4dUDCBM&uact=5&oq=the+appa%27s+cafe+&gs_lp=Egxnd3Mtd2l6LXNlcnAiEHRoZSBhcHBhJ3MgY2FmZSAyBBAjGCcyBhAAGBYYHjICECYyCxAAGIAEGIYDGIoFMggQABiABBiiBDIFEAAY7wVI2SZQsxRY9yBwAXgBkAEAmAGVAqAB8wuqAQUwLjMuNLgBA8gBAPgBAZgCBaACggfCAgoQABiwAxjWBBhHmAMAiAYBkAYIkgcFMS4yLjKgB_EesgcFMC4yLjK4B_0GwgcDMi01yAcTgAgA&sclient=gws-wiz-serp&lqi=Cg90aGUgYXBwYSdzIGNhZmVI_-7OqcK9gIAIWh0QABABEAIYABgBGAIiD3RoZSBhcHBhJ3MgY2FmZZIBC2NvZmZlZV9zaG9w#rlimm=16754513889547059541" 
          target="_blank" 
          rel="noopener noreferrer"
          className="review-link">
         ⭐ Rate Us & Add Photos Online
       </a>
-      <p>Developed By : Suraj Rathod, Om Doshi, Sanskar Tathe</p>
+         <p>&copy; 2026 The Appa's Café. All rights reserved.</p>
     </footer>
   )
 }
